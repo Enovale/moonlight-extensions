1bab0778072139315db893351a485904b52ba123
Enova <17408285+Enovale@users.noreply.github.com>
2025-07-08 19:26:51 -0500

    Add tooltip, finish port

--- src/whosWatching/webpackModules/entrypoint.ts
+++
@@ -1,8 +0,0 @@
-import { greeting } from "@moonlight-mod/wp/whosWatching_someLibrary";
-
-const logger = moonlight.getLogger("whosWatching/entrypoint");
-logger.info("Hello from entrypoint!");
-logger.info("someLibrary exports:", greeting);
-
-const natives = moonlight.getNatives("whosWatching");
-logger.info("node exports:", natives);
--- src/whosWatching/webpackModules/streamWrapper.tsx
+++ src/whosWatching/webpackModules/streamWrapper.tsx
@@ -8,7 +8,7 @@ import { User } from "@moonlight-mod/types";
 import { Clickable, FormTitle, Tooltip, FormText } from "@moonlight-mod/wp/discord/components/common/index";
 import UserSummaryItem from "@moonlight-mod/wp/discord/components/common/UserSummaryItem";
 import { openUserProfileModal } from "@moonlight-mod/wp/discord/actions/UserProfileModalActionCreators";
-import { UserStore, RelationshipStore } from "@moonlight-mod/wp/common_stores";
+import { UserStore, RelationshipStore, GuildMemberStore } from "@moonlight-mod/wp/common_stores";
 import { useStateFromStores } from "@moonlight-mod/wp/discord/packages/flux";
 import ErrorBoundary from "@moonlight-mod/wp/common_ErrorBoundary";
 import Flex from "@moonlight-mod/wp/discord/uikit/Flex";
@@ -27,8 +27,8 @@ function classes(...classes: Array<string | null | undefined | false>) {
     return classes.filter(Boolean).join(" ");
 }
 
-function getUsername(user: any): string {
-    return RelationshipStore.getNickname(user.id) || user.globalName || user.username;
+function getUsername(user: any, guildId?: string): string {
+    return RelationshipStore.getNickname(user.id) || guildId != null ? GuildMemberStore.getNick(guildId, user.id) : (user.globalName || user.username);
 }
 
 export function Watching({ userIds, guildId }: WatchingProps) {
@@ -45,7 +45,7 @@ export function Watching({ userIds, guildId }: WatchingProps) {
                         {users.map(user => (
                             <Flex direction={Flex.Direction.HORIZONTAL} style={{ gap: 6, alignContent: "center" }} className={cl("user")} >
                                 <img src={user.getAvatarURL(guildId)} style={{ borderRadius: 8, width: 16, height: 16 }} />
-                                {getUsername(user)}
+                                {getUsername(user, guildId)}
                             </Flex>
                         ))}
                         {missingUsers > 0 && <span className={cl("more_users")}>{`+${i18n.intl.format(i18n.t["3uHFUV"], { num: missingUsers })}`}</span>}
@@ -113,22 +113,32 @@ export function ScreenshareWrapper(props) {
                         <UserSummaryItem
                             users={users}
                             count={userIds.length}
+                            guildId={stream.guildId}
                             renderIcon={false}
                             max={12}
                             showDefaultAvatarsForNullUsers
                             renderMoreUsers={renderMoreUsers}
                             renderUser={(user: User) => (
-                                <Clickable
-                                    className={AvatarStyles.clickableAvatar}
-                                    onClick={() => openUserProfileModal({ userId: user.id })}
-                                >
-                                    <img
-                                        className={AvatarStyles.avatar}
-                                        src={user.getAvatarURL(void 0, 80, true)}
-                                        alt={user.username}
-                                        title={user.username}
-                                    />
-                                </Clickable>
+                                <Tooltip text={getUsername(stream.guildId, user.id)}>
+                                    {({ onMouseEnter, onMouseLeave }) => (
+                                        <div
+                                            className={cl("user_summary_icon")}
+                                            onMouseEnter={onMouseEnter}
+                                            onMouseLeave={onMouseLeave}>
+                                            <Clickable
+                                                className={AvatarStyles.clickableAvatar}
+                                                onClick={() => openUserProfileModal({ userId: user.id })}
+                                            >
+                                                <img
+                                                    className={AvatarStyles.avatar}
+                                                    src={user.getAvatarURL(void 0, 80, true)}
+                                                    alt={user.username}
+                                                    title={user.username}
+                                                />
+                                            </Clickable>
+                                        </div>
+                                    )}
+                                </Tooltip>
                             )}
                         />
                     </>
