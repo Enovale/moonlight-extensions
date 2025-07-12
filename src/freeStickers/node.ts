e2b4e7574f558904131b4974a5deedd4f2bdcf3a
Enovale <enovale@proton.me>
2025-07-12 01:01:17 -0500

    Finish freestickers, change package name

---
+++ src/freeStickers/node.ts
@@ -0,0 +1,101 @@
+// Largely taken from https://github.com/Vendicated/Vencord/blob/main/src/utils/dependencies.tsx
+
+import { applyPalette, GIFEncoder, quantize } from "gifenc";
+const { parseURL } = require("./apng-canvas").APNG as { parseURL(url: string): Promise<ApngFrameData>; };
+
+// https://wiki.mozilla.org/APNG_Specification#.60fcTL.60:_The_Frame_Control_Chunk
+const enum ApngDisposeOp {
+    /**
+     * no disposal is done on this frame before rendering the next; the contents of the output buffer are left as is.
+     */
+    NONE,
+    /**
+     * the frame's region of the output buffer is to be cleared to fully transparent black before rendering the next frame.
+     */
+    BACKGROUND,
+    /**
+     * the frame's region of the output buffer is to be reverted to the previous contents before rendering the next frame.
+     */
+    PREVIOUS
+}
+const enum ApngBlendOp {
+    SOURCE,
+    OVER
+}
+interface ApngFrame {
+    left: number;
+    top: number;
+    width: number;
+    height: number;
+    img: HTMLImageElement;
+    delay: number;
+    blendOp: ApngBlendOp;
+    disposeOp: ApngDisposeOp;
+}
+
+interface ApngFrameData {
+    width: number;
+    height: number;
+    frames: ApngFrame[];
+    playTime: number;
+}
+
+// https://moonlight-mod.github.io/ext-dev/cookbook/#extension-entrypoints
+const logger = moonlightNode.getLogger("freeStickers");
+logger.info("Hello from freeStickers/Node!");
+
+module.exports = getAnimatedStickerAsGif;
+
+async function getAnimatedStickerAsGif(stickerLink: string, stickerId: string, stickerSize: number) {
+    const { frames, width, height } = await parseURL(stickerLink);
+
+    const gif = GIFEncoder();
+    const resolution = stickerSize;
+
+    const canvas = document.createElement("canvas");
+    canvas.width = resolution;
+    canvas.height = resolution;
+
+    const ctx = canvas.getContext("2d", {
+        willReadFrequently: true
+    })!;
+
+    const scale = resolution / Math.max(width, height);
+    ctx.scale(scale, scale);
+
+    let previousFrameData: ImageData;
+
+    for (const frame of frames) {
+        const { left, top, width, height, img, delay, blendOp, disposeOp } = frame;
+
+        previousFrameData = ctx.getImageData(left, top, width, height);
+
+        if (blendOp === ApngBlendOp.SOURCE) {
+            ctx.clearRect(left, top, width, height);
+        }
+
+        ctx.drawImage(img, left, top, width, height);
+
+        const { data } = ctx.getImageData(0, 0, resolution, resolution);
+
+        const palette = quantize(data, 256);
+        const index = applyPalette(data, palette);
+
+        gif.writeFrame(index, resolution, resolution, {
+            transparent: true,
+            palette,
+            delay
+        });
+
+        if (disposeOp === ApngDisposeOp.BACKGROUND) {
+            ctx.clearRect(left, top, width, height);
+        } else if (disposeOp === ApngDisposeOp.PREVIOUS) {
+            ctx.putImageData(previousFrameData, left, top);
+        }
+    }
+
+    gif.finish();
+
+    const file = new File([gif.bytesView()], `${stickerId}.gif`, { type: "image/gif" });
+    return file;
+}
---
+++ src/freeStickers/webpackModules/entrypoint.ts
@@ -0,0 +1,94 @@
+// Heavily modified from https://github.com/uwx/moonlight-extensions/blob/master/src/freeMoji/
+
+import { SelectedGuildStore, ChannelStore, PermissionStore } from "@moonlight-mod/wp/common_stores";
+const StickerStore = spacepack.findByCode("StickerStore")[0].exports.Z;
+const DraftType = spacepack.findByCode("ChannelMessage", "SlashCommand", "ThreadSettings", "FirstThreadMessage")[0].exports.d;
+import spacepack from "@moonlight-mod/wp/spacepack_spacepack";
+
+const findStr = "=!0,showLargeMessageDialog:";
+const UploadHandler = {
+	promptToUpload: spacepack.findFunctionByStrings(spacepack.findByCode(findStr)[0].exports, findStr) as Function
+}
+
+const logger = moonlight.getLogger("freeStickers/entrypoint");
+logger.info('Hello from freeStickers/entrypoint!');
+
+const natives = moonlight.getNatives("freeStickers");
+logger.info("node exports:", natives);
+
+interface Message {
+	content: string;
+}
+
+interface Attachments {
+	stickerIds: string[] | undefined;
+}
+
+const extensions = ["json", "webp", "png", "apng", "gif", "jpeg", "jpg"];
+
+function hasPermission(channelId: string, permission: bigint) {
+	const channel = ChannelStore.getChannel(channelId);
+
+	if (!channel || channel.isPrivate()) return true;
+
+	return PermissionStore.can(permission, channel);
+}
+
+const hasAttachmentPerms = (channelId: string) => hasPermission(channelId, PermissionsBits.ATTACH_FILES);
+
+const COOL = "Queueing message to be sent";
+const module = spacepack.findByCode(COOL)[0].exports;
+
+const originalSend = module.Z.sendMessage;
+module.Z.sendMessage = async (...args: any[]) => {
+	if (modifyIfNeeded(args[0], args[1], args[3]))
+		return originalSend.call(module.Z, ...args);
+};
+
+function extractUnusableEmojis(channelId: string, stickerList: string[]) {
+	const stickerUrls = [];
+
+	for (var i = stickerList.length - 1; i >= 0; i--) {
+		const stickerId = stickerList[i];
+
+		// Fetch required info about the sticker
+		const sticker = StickerStore.getStickerById(stickerId);
+
+		// Check sticker usability
+		if (sticker.guild_id != null && sticker.guild_id !== SelectedGuildStore.getGuildId()) {
+			const format = extensions[sticker.format_type];
+
+			// Remove sticker from sticker list
+			stickerList.splice(i, 1);
+
+			// Add stickers to to send
+			const stickerSize = 160;
+			const stickerLink = `https://media.discordapp.net/stickers/${sticker.id}.${format}?size=${stickerSize}`;
+
+			if (sticker.format_type == 2 || sticker.format_type == 3) {
+				sendAnimatedSticker(channelId, stickerLink, sticker.id, stickerSize);
+			} else {
+				stickerUrls.push(stickerLink);
+			}
+		}
+	}
+	return stickerUrls;
+}
+
+async function sendAnimatedSticker(channelId: string, stickerLink: string, stickerId: string, stickerSize: number) {
+	const file = await natives(stickerLink, stickerId, stickerSize);
+	UploadHandler.promptToUpload([file], ChannelStore.getChannel(channelId), DraftType.ChannelMessage);
+}
+
+export default function modifyIfNeeded(channelId: string, msg: Message, attachments: Attachments) {
+	if (attachments.stickerIds == null || attachments.stickerIds.length <= 0) return true;
+
+	// Find all emojis from the captured message string and return object with emojiURLS and content
+	const extractedStickers = extractUnusableEmojis(channelId, attachments.stickerIds);
+
+	if (attachments.stickerIds != null && attachments.stickerIds.length <= 0 && extractedStickers.length <= 0) return false;
+
+	if (extractedStickers.length > 0) msg.content += `\n${extractedStickers.join("\n")}`;
+
+	return true;
+};
