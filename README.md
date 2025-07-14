165845267fc238cfe8c0a37e921fe1bbb540619b
Enovale <enovale@proton.me>
2025-07-14 04:13:03 -0500

    Add removeTopBar

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ All my moonlight extensions. They are largely ports of vencord plugins/userplugi
 # [Whos Watching](./src/whosWatching/README.md)
 # [USRBG](./src/usrbg/README.md)
 # [Free Stickers](./src/freeStickers/README.md)
+# [Typing Tweaks](./src/typingTweaks/README.md)
+# [Remove Title Bar](./src/removeTopBar/README.md)
---
+++ src/removeTopBar/webpackModules/entrypoint.tsx
@@ -0,0 +1,28 @@
+import ErrorBoundary from "@moonlight-mod/wp/common_ErrorBoundary";
+import React from "@moonlight-mod/wp/react";
+import spacepack from "@moonlight-mod/wp/spacepack_spacepack";
+import { inboxFind, bugFind } from "./finds";
+
+const InboxComponent = spacepack.findByCode(inboxFind)?.[0]?.exports.Z;
+const BugReportComponent = spacepack.findByCode(bugFind)?.[0]?.exports.Z;
+
+export function addIconToToolBar(e: { toolbar: React.ReactNode[] | React.ReactNode; }) {
+    if (Array.isArray(e.toolbar))
+        return e.toolbar.unshift(
+            getIcons()
+        );
+
+    e.toolbar = [
+        getIcons(),
+        e.toolbar,
+    ];
+}
+
+export function getIcons() {
+    return moonlight.getConfigOption<boolean>("removeTopBar", "moveButtons") ? (
+        <ErrorBoundary noop={true} key={"RemoveTopBar"}>
+            <InboxComponent />
+            <BugReportComponent />
+        </ErrorBoundary>
+    ) : null;
+}
