5b870baafe74a2d38a332f3764bf2015b41f393b
Cynthia Foxwell <gamers@riseup.net>
2025-07-17 20:02:25 -0600

    removeTopBar: rework logic to pull all buttons and other fixes

--- src/removeTopBar/webpackModules/entrypoint.tsx
+++ src/removeTopBar/webpackModules/entrypoint.tsx
@@ -1,27 +1,16 @@
 import ErrorBoundary from "@moonlight-mod/wp/common_ErrorBoundary";
 import React from "@moonlight-mod/wp/react";
-import spacepack from "@moonlight-mod/wp/spacepack_spacepack";
 
-const InboxComponent = spacepack.findByCode(`.getUnseenInviteCount${""}()>0)`)?.[0]?.exports.Z;
-const BugReportComponent = spacepack.findByCode('navId:"staff-help-popout"')?.[0]?.exports.Z;
-
-export function addIconToToolBar(e: { toolbar: React.ReactNode[] | React.ReactNode; }) {
-    if (Array.isArray(e.toolbar))
-        return e.toolbar.unshift(
-            getIcons()
-        );
-
-    e.toolbar = [
-        getIcons(),
-        e.toolbar,
-    ];
-}
+let buttons: Array<React.ReactNode> | undefined;
 
 export function getIcons() {
-    return moonlight.getConfigOption<boolean>("removeTopBar", "moveButtons") ? (
-        <ErrorBoundary noop={true} key={"RemoveTopBar"}>
-            <InboxComponent />
-            <BugReportComponent />
-        </ErrorBoundary>
-    ) : null;
+  return moonlight.getConfigOption<boolean>("removeTopBar", "moveButtons") && buttons != null ? (
+    <ErrorBoundary noop={true} key={"RemoveTopBar"}>
+      {buttons}
+    </ErrorBoundary>
+  ) : null;
+}
+
+export function storeButtons(incoming: Array<React.ReactNode>) {
+  buttons = incoming;
 }
