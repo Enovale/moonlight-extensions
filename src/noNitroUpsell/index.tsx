0a32f2dbf4c29b4630e20de59dc82701c16953ec
Enova <17408285+Enovale@users.noreply.github.com>
2025-07-22 05:57:18 -0500

    Overhaul noNitroUpsell

--- src/noNitroUpsell/index.tsx
+++ src/noNitroUpsell/index.tsx
@@ -2,14 +2,73 @@ import { ExtensionWebExports } from "@moonlight-mod/types";
 
 // https://moonlight-mod.github.io/ext-dev/webpack/#patching
 export const patches: ExtensionWebExports["patches"] = [
+  // I don't actually know what this is patching out
   {
     find: ".EIYbj4)", // USER_PROFILE_ENTRY_POINTS_AMP_UP_YOUR_PROFILE
+    replace:
+    {
+      match: /}\);return \i\?.*?}\)}}/,
+      replacement: "});return null}}"
+    }
+  },
+  // Insane regex to wrap the SKU feature checking function and pass the original value as well.
+  {
+    find: "ProductCatalog.tsx",
+    replace:
+    {
+      match: /(function \i\((\i),(\i)\).*let \i=(\i\[\i.premiumType\]).*let \i=(\i\[\i\]).*)return \i.skuFeatures.includes\(\i\)/,
+      replacement: (_, before, feature, user, tierList, skuSupport) => `${before}return require("noNitroUpsell_entrypoint").checkRealSkuSupport(${feature}, ${user}, ${skuSupport.replace(/\[.*\]/, `[${tierList.replace("premiumType", "_realPremiumType")}]`)})`
+    }
+  },
+  // The nitro filesize is checked separately so we must patch it explicitly
+  {
+    find: ".premiumType].fileSize",
+    replace:
+    {
+      match: /.premiumType/g,
+      replacement: "._realPremiumType"
+    }
+  },
+  // Manually patch out profile customization upsell
+  {
+    find: "UserSettingsProfileCustomization",
     replace: [
       {
-        match: /}\);return \i\?.*?}\)}}/,
-        replacement: "});return null}}"
+        match: /showUpsell:.*?,/,
+        replacement: "showUpsell: false,"
+      },
+      {
+        match: /active:(\i),.*?shouldShow:\i.*?isVisible:(\i)}/,
+        replacement: (orig, eu, el) => orig.replaceAll(eu, "false").replaceAll(el, "false")
       }
-    ],
+    ]
+  },
+  // Per-profile customization upsell
+  {
+    find: "upsellOverlay,",
+    replace:
+    {
+      match: /(showOverlay:(\i).*?;)(return\(\i\.useEffect)/,
+      replacement: (_, before, overlay, after) => `${before}${overlay} = false;${after}`
+    }
+  },
+  // Special exception to this patch for the entitlement gifts, as they are server validated
+  {
+    find: "EntitlementGifts",
+    replace:
+    {
+      match: /\(\)=>(\i\.\i\.isPremiumExactly\((\i\..*?User\(\)).*?TIER_2\))/,
+      replacement: (_, origCheck, user) => `() => require("noNitroUpsell_entrypoint").checkRealSkuSupport("canAcceptGifts", ${user}, ${origCheck})`
+    }
+  },
+  // Removes the collectibles upsell in the Profiles tab
+  {
+    find: /COLLECTIBLES_PROFILE_SETTINGS_UPSELL.*?PREMIUM_UPSELL_VIEWED/,
+    replace:
+    {
+      match: /Z:\(\)=>\i/,
+      replacement: "Z: () => () => null"
+    }
   }
 ];
 
