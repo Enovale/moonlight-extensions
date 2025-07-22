579ae2c3d4ce26b916ff3627bd3d8e9de70bedc7
Enovale <enovale@proton.me>
2025-07-22 05:57:18 -0500

    Overhaul noNitroUpsell

--- src/noNitroUpsell/webpackModules/entrypoint.tsx
+++ src/noNitroUpsell/webpackModules/entrypoint.tsx
@@ -8,6 +8,27 @@ interface ModifiedUser extends User {
   _realPremiumType?: number;
 }
 
+let exemptSkuFeatures = [
+  "soundboardEverywhere",
+  "clientThemes",
+  "profilePremiumFeatures",
+  "premiumProfileCustomization",
+  "boostDiscount",
+  "freeBoosts",
+];
+
+export function checkRealSkuSupport(feature, user, originalValue) {
+  if (feature === "canAcceptGifts") { // Special magic value defined by a patch
+    return user._realPremiumType == 2;
+  }
+
+  if (exemptSkuFeatures.includes(feature.name)) {
+    return originalValue;
+  }
+
+  return true;
+}
+
 const onChange = () => {
   const newUser = UserStore.getCurrentUser();
   if (newUser && newUser.id !== lastUserId) {
@@ -20,7 +41,7 @@ function ready(user: ModifiedUser) {
   if (!user) return;
   if ("_realPremiumType" in user) return;
 
-  user._realPremiumType = user.premiumType ?? 0;
+  user._realPremiumType = user.premiumType;
   user.premiumType = 2;
   lastUserId = user.id;
 }
