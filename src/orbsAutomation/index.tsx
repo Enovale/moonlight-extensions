d63cba2536a3cc888cc29307e0e0c3d928431928
Enova <17408285+Enovale@users.noreply.github.com>
2025-07-28 04:56:27 -0500

    Add button to automate quests

--- src/orbsAutomation/index.tsx
+++ src/orbsAutomation/index.tsx
@@ -9,7 +9,27 @@ export const patches: ExtensionWebExports["patches"] = [
       match: /videoInner.*?\}\),/,
       replacement: (orig) => `${orig}style: {display: 'none'},`
     }
-  }
+  },
+  {
+    find: "questEnrollmentBlockedUntil}))",
+    replace: [
+      // Make quest var accessible
+      {
+        match: /let\{quest:(\i),(.*?=\i,)/,
+        replacement: (_, quest, middle) => `let{quest: ${quest},${middle}questVar = ${quest},`
+      },
+      // Replace button arrays
+      {
+        match: /(\.Fragment,.*?)\[(\(.*?children:\i}\))\]/,
+        replacement: (_, before, after) => `${before}[${after}, require("orbsAutomation_entrypoint").SpoofButton(questVar)]`,
+      },
+      // Replace single buttons
+      {
+        match: /(\(0,\i\.jsx\).{1,50}?\.BRAND,.{1,50}?\i\.button,.{1,50}?children:\i}\)):/g,
+        replacement: (_, self) => `require("orbsAutomation_entrypoint").SpoofButton(questVar, ${self}):`
+      }
+    ]
+  },
 ];
 
 // https://moonlight-mod.github.io/ext-dev/webpack/#webpack-module-insertion
@@ -18,6 +38,8 @@ export const webpackModules: ExtensionWebExports["webpackModules"] = {
     dependencies: [
       { ext: "spacepack", id: "spacepack" },
       { ext: "common", id: "stores" },
+      { id: "discord/components/common/index" },
+      { id: "discord/components/common/PanelButton" },
       { id: "discord/Dispatcher" },
       { id: "discord/utils/HTTPUtils" },
     ],
