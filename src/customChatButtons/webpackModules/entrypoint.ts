20d30e2ac9bf0d636be8b6a5423ebe729a623ea5
Enova <17408285+Enovale@users.noreply.github.com>
2025-08-04 09:53:56 -0500

    customChatButtons now on par with meow

--- src/customChatButtons/webpackModules/entrypoint.ts
+++
@@ -1,8 +0,0 @@
-import { greeting } from "@moonlight-mod/wp/customChatButtons_someLibrary";
-
-const logger = moonlight.getLogger("customChatButtons/entrypoint");
-logger.info("Hello from entrypoint!");
-logger.info("someLibrary exports:", greeting);
-
-const natives = moonlight.getNatives("customChatButtons");
-logger.info("node exports:", natives);
