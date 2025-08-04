e98ff7de9b4fa28aea85ad56ddbe3c7700280032
Enovale <enovale@proton.me>
2025-08-04 09:53:56 -0500

    customChatButtons now on par with meow

--- src/customChatButtons/index.tsx
+++ src/customChatButtons/index.tsx
@@ -1,29 +1,18 @@
 import { ExtensionWebExports } from "@moonlight-mod/types";
 
-// https://moonlight-mod.github.io/ext-dev/webpack/#patching
-export const patches: ExtensionWebExports["patches"] = [
-  {
-    find: /"User Settings",/g,
-    replace: {
-      match: /"User Settings",/g,
-      replacement: '"hacked by customChatButtons lol",'
-    }
-  }
-];
-
 // https://moonlight-mod.github.io/ext-dev/webpack/#webpack-module-insertion
 export const webpackModules: ExtensionWebExports["webpackModules"] = {
   entrypoint: {
     dependencies: [
-      {
-        ext: "customChatButtons",
-        id: "someLibrary"
-      }
+      { ext: "spacepack", id: "spacepack" },
+      { ext: "componentEditor", id: "chatButtonList" },
+      { ext: "common", id: "stores" },
+      { ext: "common", id: "ErrorBoundary" },
+      { id: "discord/components/common/index" },
+      "CHAT_INPUT_BUTTON_NOTIFICATION,width",
+      ",spriteContainer:",
+      "discord/actions/MessageActionCreators"
     ],
     entrypoint: true
-  },
-
-  someLibrary: {
-    // Keep this object, even if it's empty! It's required for the module to be loaded.
   }
 };
