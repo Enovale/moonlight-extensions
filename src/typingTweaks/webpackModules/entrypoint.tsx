4c7ca816ecf734dccdf50f0bfce6a9a37c265a21
Enova <17408285+Enovale@users.noreply.github.com>
2025-08-07 03:33:58 -0500

    Backport typingTweaks fix

--- src/typingTweaks/webpackModules/entrypoint.tsx
+++ src/typingTweaks/webpackModules/entrypoint.tsx
@@ -1,9 +1,12 @@
-import { GuildMemberStore, RelationshipStore } from "@moonlight-mod/wp/common_stores";
+import { GuildMemberStore, RelationshipStore, TypingStore, UserStore, AuthenticationStore } from "@moonlight-mod/wp/common_stores";
+import { useStateFromStores } from "@moonlight-mod/wp/discord/packages/flux";
 import ErrorBoundary from "@moonlight-mod/wp/common_ErrorBoundary";
 import React from "@moonlight-mod/wp/react";
-import { User } from "@moonlight-mod/types";
+import { User, Channel } from "@moonlight-mod/types";
 import { Avatar } from "@moonlight-mod/wp/discord/components/common/index";
 import { openUserProfileModal } from "@moonlight-mod/wp/discord/actions/UserProfileModalActionCreators";
+
+const logger = moonlight.getLogger("typingTweaks/entrypoint");
 
 interface TypingUserProps {
     user: User;
@@ -11,8 +14,7 @@ interface TypingUserProps {
 }
 
 interface SeveralTypingUsersProps {
-    a: User;
-    b: User;
+    users: User[];
     count: number;
     guildId: string;
 }
@@ -53,13 +55,15 @@ function TypingUser({ user, guildId }: TypingUserProps) {
     );
 }
 
-export function buildSeveralUsers({ a, b, count, guildId }: SeveralTypingUsersProps) {
+export function buildSeveralUsers({ users, count, guildId }: SeveralTypingUsersProps) {
     return (
         <ErrorBoundary noop={true}>
-            <TypingUser user={a} guildId={guildId} />
-            {", "}
-            <TypingUser user={b} guildId={guildId} />
-            {", "}
+            {users.slice(0, count).map(user => (
+                <React.Fragment key={user.id}>
+                    <TypingUser user={user} guildId={guildId} />
+                    {", "}
+                </React.Fragment>
+            ))}
             and {count} others are typing...
         </ErrorBoundary>
     );
@@ -73,16 +77,37 @@ export function renderTypingUsers({ guildId, users, children }: React.PropsWithC
 
         let element = 0;
 
-        return children.map(c => {
-            if (c.type !== "strong" && !(typeof c !== "string" && !React.isValidElement(c)))
-                return c;
+        return <ErrorBoundary noop={true}>
+            {children.map(c => {
+                if (c.type !== "strong" && !(typeof c !== "string" && !React.isValidElement(c)))
+                    return c;
 
-            const user = users[element++];
-            return <TypingUser key={user.id} guildId={guildId} user={user} />;
-        });
+                const user = users[element++];
+                return <TypingUser key={user.id} guildId={guildId} user={user} />;
+            })}
+        </ErrorBoundary>
     } catch (e) {
-        console.error(e);
+        logger.error(e);
     }
 
     return <ErrorBoundary noop={true} children={children} />;
+}
+
+export function useTypingUsers(channel: Channel | undefined): User[] {
+    try {
+        if (!channel) {
+            throw new Error("No channel");
+        }
+
+        const typingUsers = useStateFromStores([TypingStore], () => TypingStore.getTypingUsers(channel.id));
+        const myId = useStateFromStores([AuthenticationStore], () => AuthenticationStore.getId());
+
+        return Object.keys(typingUsers)
+            .filter(id => id && (moonlight.getConfigOption<boolean>("typingTweaks", "showSelfTyping") || id !== myId) && !RelationshipStore.isBlockedOrIgnored(id))
+            .map(id => UserStore.getUser(id))
+            .filter(u => u != null);
+    } catch (e) {
+        logger.error("Failed to get typing users:", e);
+        return [];
+    }
 }
