19fb7c4e8c69cd1878ebd6ffd44b973618426e56
Enovale <enovale@proton.me>
2025-08-11 06:52:56 -0500

    Allow putting raw image data in the icon config

--- src/customChatButtons/node.ts
+++ src/customChatButtons/node.ts
@@ -7,19 +7,30 @@ const logger = moonlightNode.getLogger("customChatButtons/node");
 
 export function getImageData(file: string) {
 	let path = resolve(file);
-	if (!fs.existsSync(path)) {
-		return { data: null, isSvg: false };
-	}
-
+	
 	try {
-		let type = mime.lookup(path);
-		let typeIsSvg = type == "image/svg+xml";
-		let data = fs.readFileSync(path, typeIsSvg ? "utf-8" : undefined);
+		let type, typeIsSvg, data;
+
+		if (!fs.existsSync(path)) {
+			// Assume a raw SVG/dataurl is being used
+			if (file.startsWith("data:"))
+				return { data: file, isSvg: false };
+
+			if (!isSvg(file, { validate: false }))
+				return { data: null, isSvg: false };
+
+			typeIsSvg = true;
+			data = file;
+		} else {
+			type = mime.lookup(path);
+			typeIsSvg = type == "image/svg+xml" || (typeof (data) === "string" && isSvg(data, { validate: false }));
+			data = fs.readFileSync(path, typeIsSvg ? "utf-8" : undefined);
+		}
 
 		if (!data)
 			return null;
 
-		if (typeIsSvg || (typeof(data) === "string" && isSvg(data, { validate: false }))) {
+		if (typeIsSvg) {
 			return { data: data, isSvg: true };
 		} else {
 			return { data: `data:${type};base64,${data.toString('base64')}`, isSvg: false };
